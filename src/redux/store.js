8ab873d484723ca3bee95305e3d2a75ddfd148e3
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-12-22 13:52:49 +0000

    auth folder

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,17 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import contactsReducer from "./contactsSlice";
-import filtersReducer from "./filtersSlice.js";
+import filtersReducer from "./filtersSlice";
+import authReducer from "./auth/slice";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   contacts: contactsReducer,
   filters: filtersReducer,
+  auth: authReducer,
 });
 
 const persistConfig = {
   key: "root",
   storage,
+  whitelist: ["auth"],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
